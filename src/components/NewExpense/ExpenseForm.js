bfa92ac3cd52b621982717bd083db01883294d66
Anuja Sharma <anuja@DESKTOP-FGIBIOB.(none)>
2022-05-28 05:00:15 -0500

    fixed a bug

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -46,7 +46,7 @@ const[enteredDate, setEnteredDate] = useState('');
     
         const expenseData = {
             title: enteredTitle,
-            amount : enteredAmount,
+            amount : +enteredAmount,
             date: new Date(enteredDate),
         };
             props.onSaveExpenseData(expenseData);
